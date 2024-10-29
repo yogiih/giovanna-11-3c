02c9f60645ebb9441816ac6c33d134a222997e31
Giih <103268381+yogiih@users.noreply.github.com>
2024-10-29 07:58:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 const grafico = document.getElementById("meuGrafico").getContext("2d");
-const meuGrafico = new Chart (Grafico, {
-type: "pie",
-data: {
-    labels: [
-        "Instagram",
-        "WhatsApp",
-        "Facebook",
-        "WeChat",
-        "TikTok",
-        "DouYin",
-        "Twitter",
-        "Telegram",
-        "FB",
-        "Messenger",
-        "LINE"
+const meuGrafico = new Chart (grafico, {
+  type: "pie",
+  data: {
+    labels:[
+      "Instagram",
+      "WhatsApp",
+      "Facebook",
+      "WeChat",
+      "TikTok",
+      "DouYin",
+      "Twitter",
+      "Telegram",
+      "FB Messenger",
+      "LINE"
     ],
+  }
 }
-}
-)

745a89eadf96b1d5c74567d24e97b7e2cfd5adb0
Giih <103268381+yogiih@users.noreply.github.com>
2024-10-30 10:44:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 const grafico = document.getElementById("meuGrafico").getContext("2d");
-const meuGrafico = new Chart (Grafico, {
-type: "pie",
-data: {
-    labels: [
-        "Instagram",
-        "WhatsApp",
-        "Facebook",
-        "WeChat",
-        "TikTok",
-        "DouYin",
-        "Twitter",
-        "Telegram",
-        "FB",
-        "Messenger",
+const meuGrafico = new Chart(grafico, {
+    type: "pie",
+    data: {
+      labels: [
+        "Instagram", 
+        "WhatsApp", 
+        "Facebook", 
+        "WeChat", 
+        "TikTok", 
+        "DouYin", 
+        "Twitter", 
+        "Telegram", 
+        "FB Messenger", 
         "LINE"
-    ],
-    datasets:[{
-        data:[
-            16.5,
-            16.1,
-            12.8,
-            12.8,
-            7.4,
-            6.6,
-            3.2,
-            2.4,
-            2.3,
-            1.7,
+      ],
+      datasets: [{
+        data: [
+          16.5, 
+          16.1, 
+          12.8, 
+          12.8, 
+          7.4, 
+          6.6, 
+          3.2, 
+          2.4, 
+          2.3, 
+          1.7
         ],
-        backgroundColor:[
-            "rgb(#FFD700)",
-            "rgb(#C0C0C0)",
-            "rgb(#A97142)",
-            "rgb(#006400)",
-            "rgb(#008000)",
-            "rgb(#228B22)",
-            "rgb(#32CD32)",
-            "rgb(#00FF00)",
-            "rgb(#7CFC00)",
-            "rgb(#7FFF00)",
+        backgroundColor: [
+          "red", 
+          "blue",
+          "green",
+          "#ADFF2F",
+          "#DAA520",
+          "#BC8F8F",
+          "rgb(245,222,179)",
+          "rgb(186,85,211)",
+          "rgb(255,20,147)",
+          "CadetBlue",        
         ],
-    }]
-}
-}
-)
+      }
+    ]
+    }
+  }
+);

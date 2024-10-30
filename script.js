368b034482e5115ec55b507ddd1b76c96ba719ac
Giih <103268381+yogiih@users.noreply.github.com>
2024-10-30 10:38:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 const grafico = document.getElementById("meuGrafico").getContext("2d");
-const meuGrafico = new Chart (grafico, {
-  type: "pie",
-  data: {
-    labels:[
-      "Instagram",
-      "WhatsApp",
-      "Facebook",
-      "WeChat",
-      "TikTok",
-      "DouYin",
-      "Twitter",
-      "Telegram",
-      "FB Messenger",
-      "LINE"
+const meuGrafico = new Chart (Grafico, {
+type: "pie",
+data: {
+    labels: [
+        "Instagram",
+        "WhatsApp",
+        "Facebook",
+        "WeChat",
+        "TikTok",
+        "DouYin",
+        "Twitter",
+        "Telegram",
+        "FB",
+        "Messenger",
+        "LINE"
     ],
-  }
+    datasets:[{
+        data:[
+            16.5,
+            16.1,
+            12.8,
+            12.8,
+            7.4,
+            6.6,
+            3.2,
+            2.4,
+            2.3,
+            1.7,
+        ],
+        backgroundColor:[
+            "rgb(#FFD700)",
+            "rgb(#C0C0C0)",
+            "rgb(#A97142)",
+            "rgb(#006400)",
+            "rgb(#008000)",
+            "rgb(#228B22)",
+            "rgb(#32CD32)",
+            "rgb(#00FF00)",
+            "rgb(#7CFC00)",
+            "rgb(#7FFF00)",
+        ],
+    }]
 }
+}
+)
